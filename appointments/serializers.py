9cc8ac8be20c09337964d180908b8f28bd967241
puneetarora2000 <puneetarora2000@gmail.com>
2016-08-25 13:04:25 +0530

    Device-Patient Registration Serializer'

---
+++ appointments/serializers.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import User, Group
+from appointments.models import  Appointment
+from rest_framework import serializers
+
+
+
+#1
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'groups')
+
+#2
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('url', 'name')
+#3
+class AppointmentSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model =  Appointment
+        fields = ('contact','start_datetime','end_datetime','measurement','interval','lastupdate')
+
+#
